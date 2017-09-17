6192dac9d818d978b553c4cb6443521d92895542
20143104 <csh4096@gmail.cogit config --global user.email csh4096@gmail.com>
2017-09-17 23:29:47 +0900

    final

---
+++ gametest/Assets/04.Scripts/Main/MainManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("Play");
+    }
+    public void OnEndClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ gametest/Assets/04.Scripts/Play/New Folder/Enemy_Control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Control : MonoBehaviour {
+
+    public int hp = 5;
+    public int initHp = 5;
+    public Transform tr;
+    public GameObject effect;
+    public float destroyXpos;
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("PlayerMissile"))
+        {
+            hp--;
+            if (hp == 0)
+            {
+                //scoring
+                Instantiate(effect, tr.position, Quaternion.identity);
+                Destroy(this.gameObject);
+
+            }
+        }
+    }
+    private void Update()
+    {
+        if (tr.position.x < destroyXpos)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
